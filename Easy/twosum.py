0abb432bb9bd9d653197ac20d1d61fecac804cb8
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-05-25 13:36:58 +0800

    https://leetcode.com/problems/two-sum/

--- Easy/twosum.py
+++ Easy/twosum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indices = {} #indices[a] stores the index of element a in nums
+        indices = {} #indices[b] stores the index of element b in nums
         for key,value in enumerate(nums):
             if target - value in indices:
                 return [key,indices[target-value]]
