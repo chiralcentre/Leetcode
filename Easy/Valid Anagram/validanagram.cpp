ea154bc6da27b2c8b87819f9167d5dc31fddfcad
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-05-26 20:24:59 +0800

    +

---
+++ Easy/Valid Anagram/validanagram.cpp
@@ -0,0 +1,23 @@
+#include <unordered_map> //hash table DS
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char,int> letters;
+        for (char &ch: s){
+            if (letters.count(ch) == 1) letters[ch]++;
+            else letters[ch] = 1;
+        }
+        for (char &ch: t){
+            if (letters.count(ch) == 1) letters[ch]--;
+            else return false;
+        }
+        for (auto &pair: letters){
+            if (pair.second != 0) return false;
+        }
+        return true;
+    }
+};
