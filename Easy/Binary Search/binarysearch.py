20e3cbeb2c9733d8f8a3ffea72b7925bc9f4e2ef
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-05-26 12:16:48 +0800

    https://leetcode.com/problems/binary-search/

--- Easy/Binary Search/binarysearch.py
+++ Easy/Binary Search/binarysearch.py
@@ -1,18 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binary_search(arr,x):
-            low,mid,high = 0,0,len(arr)-1
-            while low <= high:
-                mid = (high + low) // 2
-                # If x is greater, ignore left half
-                if arr[mid] < x:
-                    low = mid + 1
-                # If x is smaller, ignore right half
-                elif arr[mid] > x:
-                    high = mid - 1
-                # x is present at mid
-                else:
-                    return mid
-            # the element was not present
-            return -1
-        return binary_search(nums,target)
+        low,mid,high = 0,0,len(nums)-1
+        while low <= high:
+            mid = (high + low) // 2
+            # If target is greater, ignore left half
+            if nums[mid] < target:
+                low = mid + 1
+            # If target is smaller, ignore right half
+            elif nums[mid] > target:
+                high = mid - 1
+            # target is present at mid
+            else:
+                return mid
+        # target was not present
+        return -1
+
